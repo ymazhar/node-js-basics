5559120253c058036ed07578b21035a06d84ab3e
Yaroslav Mazhar <yaroslav.mazhar@pearson.com>
2022-04-30 19:53:47 +0300

    add task_1 from homework_1

---
+++ task1.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.on('line', function(line ){
+    const reverseLine = line.split('').reverse().join('')
+
+    console.log(reverseLine);
+})
+
+
+
+
+
