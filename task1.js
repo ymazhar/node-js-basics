e97570fa5cca2f47e52f7c8ee0901ebe97f69413
Yaroslav Mazhar <yaroslav.mazhar@pearson.com>
2022-04-30 20:01:41 +0300

    add package.json

--- task1.js
+++ task1.js
@@ -1,16 +1,11 @@
-const readline = require('readline');
+const readline = require('readline')
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-});
+})
 
 rl.on('line', function(line ){
     const reverseLine = line.split('').reverse().join('')
 
-    console.log(reverseLine);
+    console.log(reverseLine)
 })
-
-
-
-
-
