4145ff99794dea323a5f0033d9387e7a4dbc00bc
Yaroslav Mazhar <yaroslav.mazhar@pearson.com>
2022-05-08 20:23:42 +0300

    fix task2 after review

--- task2.js
+++ task2.js
@@ -1,17 +1,28 @@
 const csvFilePath = './csv/nodejs-hw1-ex1.csv'
+const txtFilePath = './out.txt'
 import csv from 'csvtojson'
 import fs from 'fs'
 
 const readStream = fs.createReadStream(csvFilePath)
-const writeStream = fs.createWriteStream('./out.txt', 'utf8')
+const writeStream = fs.createWriteStream(txtFilePath, 'utf8')
 
-const handleError = () => {
+const handleError = (error) => {
     readStream.destroy()
-    writeStream.end('Finished with error...')
+    csv().destroy()
+    console.error(error.message)
 }
 
 readStream
-    .on('error', handleError)
-    .pipe(csv())
+    .on('error', (error) => handleError(error))
+    .pipe(csv({
+        colParser: {
+            "Amount": "omit",
+            "Price": "number",
+        },
+        checkType: true
+    }))
+    .subscribe((json) => {
+        console.log(json);
+    })
     .pipe(writeStream)
-    .on('error', handleError)
+    .on('error', (error) => handleError(error))
